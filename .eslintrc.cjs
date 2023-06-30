c916626a22d0740da78b0b38c7b4875082d25256
costadaniel <dancosta0601@gmail.com>
2023-06-30 12:41:24 -0300

    revert(configuration file): update the eslint configuration files
    Add a propper eslint configuration file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,10 +8,6 @@ module.exports = {
       parser: "astro-eslint-parser",
       // Parse the script in `.astro` as TypeScript by adding the following configuration.
       // It's the setting you need when using TypeScript.
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
-      },
       rules: {
         // override/add rules settings here, such as:
         // "astro/no-set-html-directive": "error"
